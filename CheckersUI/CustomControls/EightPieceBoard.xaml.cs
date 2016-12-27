15dd1232c0551286774f7b1a700f6952f96b2ad1
Hosch250 <hosch250@live.com>
2016-12-26 20:04:09 -0600

    Enable board flipping

--- CheckersUI/CustomControls/EightPieceBoard.xaml.cs
+++ CheckersUI/CustomControls/EightPieceBoard.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -7,8 +6,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media.Imaging;
 using CheckersUI.Facade;
 using Windows.Storage;
-using System.Threading.Tasks;
 using Windows.UI.Core;
+using Windows.UI.Xaml.Media;
 
 namespace CheckersUI.CustomControls
 {
@@ -17,22 +16,46 @@ namespace CheckersUI.CustomControls
         private ApplicationDataContainer _roamingSettings = ApplicationData.Current.RoamingSettings;
 
         public static readonly DependencyProperty BoardProperty =
-            DependencyProperty.Register(nameof(Board), typeof(Board), typeof(EightPieceBoard), new PropertyMetadata(null, (sender, e) => ((EightPieceBoard)sender).LoadPieces(e.OldValue as Board, e.NewValue as Board)));
+            DependencyProperty.Register(nameof(Board), typeof(Board), typeof(EightPieceBoard),
+                new PropertyMetadata(null, (sender, e) => ((EightPieceBoard)sender).LoadPieces(e.OldValue as Board, e.NewValue as Board)));
 
         public static readonly DependencyProperty SelectionProperty =
             DependencyProperty.Register(nameof(Selection), typeof(Coord), typeof(EightPieceBoard), null);
 
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register(nameof(Orientation), typeof(Player), typeof(EightPieceBoard),
+                new PropertyMetadata(null, (sender, e) => ((EightPieceBoard)sender).LoadBoard((Player)e.NewValue)));
+
         public EightPieceBoard()
         {
             InitializeComponent();
-            LoadBoard();
 
             _currentTheme = (string)_roamingSettings.Values["Theme"];
             ApplicationData.Current.DataChanged += Current_DataChanged;
         }
 
+        public Board Board
+        {
+            get { return (Board)GetValue(BoardProperty); }
+            set { SetValue(BoardProperty, value); }
+        }
+
+        public Coord Selection
+        {
+            get { return (Coord)GetValue(SelectionProperty); }
+            set { SetValue(SelectionProperty, value); }
+        }
+
+        public Player Orientation
+        {
+            get { return GetValue(OrientationProperty) is Player ? (Player)GetValue(OrientationProperty) : Player.Black; }
+            set { SetValue(OrientationProperty, value); }
+        }
+
         private Uri GetPieceUri(Piece piece)
         {
+            if (piece == null) { return null; }
+
             if (piece.Equals(Piece.WhiteChecker))
             {
                 return new Uri($"ms-appx:///../Assets/{_roamingSettings.Values["Theme"]}Theme/WhiteChecker.png", UriKind.Absolute);
@@ -68,18 +91,30 @@ namespace CheckersUI.CustomControls
             }
         }
 
-        private void LoadBoard()
+        private void LoadBoard(Player player)
         {
             var uri = new Uri($"ms-appx:///../Assets/{_roamingSettings.Values["Theme"]}Theme/Checkerboard.png", UriKind.Absolute);
             var bitmapImage = new BitmapImage { UriSource = uri };
 
             var image = new Image { Source = bitmapImage };
             image.Name = "BoardImage";
+
+            if (player == Player.Black)
+            {
+                image.RenderTransformOrigin = new Windows.Foundation.Point { X = 0.5, Y = 0.5 };
+                image.RenderTransform = new ScaleTransform() { ScaleY = -1 };
+            }
+
             Grid.SetRowSpan(image, 8);
             Grid.SetColumnSpan(image, 8);
 
             DeleteBoard();
             BoardGrid.Children.Add(image);
+
+            if (Board != null)
+            {
+                LoadPieces(null, Board);
+            }
         }
 
         private string _currentTheme;
@@ -93,21 +128,8 @@ namespace CheckersUI.CustomControls
             Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
                 _currentTheme = (string)_roamingSettings.Values["Theme"];
-                LoadBoard();
-                LoadPieces(null, Board);
+                LoadBoard(Orientation);
             });
-        }
-
-        public Board Board
-        {
-            get { return (Board)GetValue(BoardProperty); }
-            set { SetValue(BoardProperty, value); }
-        }
-
-        public Coord Selection
-        {
-            get { return (Coord)GetValue(SelectionProperty); }
-            set { SetValue(SelectionProperty, value); }
         }
 
         private void BoardGrid_PointerPressed(object sender, PointerRoutedEventArgs e)
@@ -117,20 +139,20 @@ namespace CheckersUI.CustomControls
             var row = (int)Math.Floor(point.Y / (BoardGrid.ActualHeight / 8));
             var column = (int)Math.Floor(point.X / (BoardGrid.ActualWidth / 8));
 
-            Selection = new Coord(row, column);
+            Selection = new Coord(AdjustedIndex(row), AdjustedIndex(column));
         }
 
-        private void ClearPieces(Board board)
+        private void ClearPieces()
         {
             foreach (Image element in BoardGrid.Children.ToList())
             {
                 if (element.Name == "BoardImage") { continue; }
 
-                var row = Grid.GetRow(element);
-                var column = Grid.GetColumn(element);
+                var row = AdjustedIndex(Grid.GetRow(element));
+                var column = AdjustedIndex(Grid.GetColumn(element));
                 var uri = ((BitmapImage)element.Source).UriSource.AbsolutePath;
 
-                if (Board.GameBoard[row][column] == null || GetPieceUri(Board.GameBoard[row][column]).AbsolutePath != uri)
+                if (GetPieceUri(Board.GameBoard[row][column])?.AbsolutePath != uri)
                 {
                     BoardGrid.Children.Remove(element);
                 }
@@ -149,21 +171,26 @@ namespace CheckersUI.CustomControls
 
         public void LoadPieces(Board oldValue, Board newValue)
         {
-            ClearPieces(Board);
+            if (newValue == null) { return; }
+
+            ClearPieces();
 
             for (var rowIndex = 0; rowIndex < Board.GameBoard.Count; rowIndex++)
             {
                 for (var colIndex = 0; colIndex < Board.GameBoard[rowIndex].Count; colIndex++)
                 {
-                    var piece = Board.GameBoard[rowIndex][colIndex];
-                    if (piece == null || newValue.GameBoard[rowIndex][colIndex].Equals(oldValue?.GameBoard[rowIndex][colIndex]))
+                    var piece = newValue.GameBoard[rowIndex][colIndex];
+                    if (piece == null || piece.Equals(oldValue?.GameBoard[rowIndex][colIndex]))
                     {
                         continue;
                     }
-
-                    PlaceChecker(piece, rowIndex, colIndex);
+                    
+                    PlaceChecker(piece, AdjustedIndex(rowIndex), AdjustedIndex(colIndex));
                 }
             }
         }
+
+        private int AdjustedIndex(int index) =>
+            Orientation == Player.White ? index : 7 - index;
     }
 }
