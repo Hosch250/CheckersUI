28ec56c931239d369494b30285052fba0be86cb3
Abraham Hosch <hosch250@live.com>
2017-01-06 21:00:44 -0600

    Hide white move's button if there is no move

---
+++ CheckersUI/Converters/PDNMoveToVisibility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using CheckersUI.Facade;
+
+namespace CheckersUI.Converters
+{
+    public class PDNMoveToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) =>
+            (PDNMove) value == null ? Visibility.Collapsed : Visibility.Visible;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
