76a35bdd0690d3d40c36c7f349397a31f992f878
Abraham Hosch <hosch250@live.com>
2017-01-11 19:25:59 -0600

    Enum-to-string converters

---
+++ CheckersUI/Converters/VariantToDisplayString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using CheckersUI.Facade;
+
+namespace CheckersUI.Converters
+{
+    public class VariantToDisplayStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var variant = (Variant)value;
+            switch (variant)
+            {
+                case Variant.AmericanCheckers:
+                    return "American Checkers";
+                case Variant.PoolCheckers:
+                    return "Pool Checkers";
+                default:
+                    throw new ArgumentException(nameof(variant));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var str = (string)value;
+            switch (str)
+            {
+                case "American Checkers":
+                    return Variant.AmericanCheckers;
+                case "Pool Checkers":
+                    return Variant.PoolCheckers;
+                default:
+                    throw new ArgumentException(nameof(str));
+            }
+        }
+    }
+}
---
+++ CheckersUI/Converters/VariantsToDisplayStrings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml.Data;
+using CheckersUI.Facade;
+
+namespace CheckersUI.Converters
+{
+    public class VariantsToDisplayStringsConverter : IValueConverter
+    {
+        private string VariantToString(Variant variant)
+        {
+            switch (variant)
+            {
+                case Variant.AmericanCheckers:
+                    return "American Checkers";
+                case Variant.PoolCheckers:
+                    return "Pool Checkers";
+                default:
+                    throw new ArgumentException(nameof(variant));
+            }
+        }
+
+        private Variant StringToVariant(string str)
+        {
+            switch (str)
+            {
+                case "American Checkers":
+                    return Variant.AmericanCheckers;
+                case "Pool Checkers":
+                    return Variant.PoolCheckers;
+                default:
+                    throw new ArgumentException(nameof(str));
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var variants = (IEnumerable<Variant>)value;
+            return variants.Select(VariantToString);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var strings = (IEnumerable<string>)value;
+            return strings.Select(StringToVariant);
+        }
+    }
+}
