ede2e07bc5b277306aba2299361acfb13584da21
Abraham Hosch <hosch250@live.com>
2017-01-19 20:04:47 -0600

    Add unit tests

--- TestLibrary/Tests/GamePageTests.cs
+++ TestLibrary/Tests/GamePageTests.cs
@@ -2,6 +2,7 @@
 using CheckersUI.VMs;
 using Xunit;
 
+#pragma warning disable VSD0025 // Implements the most common configuration of naming conventions.
 namespace TestLibrary.Tests
 {
     public class GamePageTests
@@ -9,14 +10,15 @@ namespace TestLibrary.Tests
         [Fact]
         public void Undo_UndoesMove()
         {
-            var vm = new GamePageViewModel();
-            var controller = new GameController(Variant.AmericanCheckers)
-                .Move(new Coord(2, 1), new Coord(3, 0))
-                .Move(new Coord(5, 6), new Coord(4, 7));
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers)
+                    .Move(new Coord(2, 1), new Coord(3, 0))
+                    .Move(new Coord(5, 6), new Coord(4, 7)),
+                BlackOpponent = CheckersUI.Opponent.Human,
+                WhiteOpponent = CheckersUI.Opponent.Human
+            };
 
-            vm.Controller = controller;
-            vm.BlackOpponent = CheckersUI.Opponent.Human;
-            vm.WhiteOpponent = CheckersUI.Opponent.Human;
 
             Assert.Equal(Piece.WhiteChecker, vm.Controller.Board[4, 7]);
 
@@ -28,20 +30,172 @@ namespace TestLibrary.Tests
         [Fact]
         public void Undo_UndoesMoveHistory()
         {
-            var vm = new GamePageViewModel();
-            var controller = new GameController(Variant.AmericanCheckers)
-                .Move(new Coord(2, 1), new Coord(3, 0))
-                .Move(new Coord(5, 6), new Coord(4, 7));
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers)
+                    .Move(new Coord(2, 1), new Coord(3, 0))
+                    .Move(new Coord(5, 6), new Coord(4, 7)),
+                BlackOpponent = CheckersUI.Opponent.Human,
+                WhiteOpponent = CheckersUI.Opponent.Human
+            };
 
-            vm.Controller = controller;
-            vm.BlackOpponent = CheckersUI.Opponent.Human;
-            vm.WhiteOpponent = CheckersUI.Opponent.Human;
 
             Assert.NotNull(vm.Controller.MoveHistory[0].WhiteMove);
 
             vm.UndoMoveCommand.Execute(null);
 
             Assert.Null(vm.Controller.MoveHistory[0].WhiteMove);
+        }
+
+        [Fact]
+        public void ToggleSettingsDisplay_TurnsOn()
+        {
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers),
+                DisplaySettingsGrid = false
+            };
+
+            vm.ToggleDisplaySettingsCommand.Execute(null);
+
+            Assert.True(vm.DisplaySettingsGrid);
+        }
+
+        [Fact]
+        public void ToggleSettingsDisplay_TurnsOff()
+        {
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers),
+                DisplaySettingsGrid = true
+            };
+
+            vm.ToggleDisplaySettingsCommand.Execute(null);
+
+            Assert.False(vm.DisplaySettingsGrid);
+        }
+
+        [Fact]
+        public void NewGameDisplay_TurnsOn()
+        {
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers),
+                DisplayCreateGameGrid = false
+            };
+
+            vm.DisplayCreateGameCommand.Execute(null);
+
+            Assert.True(vm.DisplayCreateGameGrid);
+        }
+
+        [Fact]
+        public void CreateGame_HidesNewGameDisplay()
+        {
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers),
+                DisplayCreateGameGrid = true
+            };
+
+            vm.CreateGameCommand.Execute(null);
+
+            Assert.False(vm.DisplayCreateGameGrid);
+        }
+
+        [Fact]
+        public void CreateGame_SetsGameController()
+        {
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers)
+                    .Move(new Coord(2, 1), new Coord(3, 0))
+                    .Move(new Coord(5, 6), new Coord(4, 7))
+            };
+
+            Assert.NotEmpty(vm.Controller.MoveHistory);
+
+            vm.CreateGameCommand.Execute(null);
+
+            Assert.Empty(vm.Controller.MoveHistory);
+        }
+
+        [Fact]
+        public void CreateGame_SetsGameController_FromPosition_CreatesControllerCorrectly()
+        {
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers),
+                SetupOption = CheckersUI.Setup.FromPosition
+            };
+
+            var expectedFen = @"[FEN ""B:W20,21,22,23,25,26,27,28,29,30,31,32:B1,2,3,4,5,6,7,8,10,11,12,13""]";
+            vm.CreateGameCommand.Execute(expectedFen);
+
+            Assert.Equal(expectedFen, vm.Controller.Fen);
+        }
+
+        [Fact]
+        public void CreateGame_SetsGameController_FromPosition_EmptyString_CreatesControllerCorrectly()
+        {
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers),
+                SetupOption = CheckersUI.Setup.FromPosition
+            };
+
+            var expectedFen = @"[FEN ""B:W21,22,23,24,25,26,27,28,29,30,31,32:B1,2,3,4,5,6,7,8,9,10,11,12""]";
+            vm.CreateGameCommand.Execute(expectedFen);
+
+            Assert.Equal(expectedFen, vm.Controller.Fen);
+        }
+
+        [Fact]
+        public void CancelGame()
+        {
+            var vm = new GamePageViewModel {Controller = new GameController(Variant.AmericanCheckers)};
+
+            Assert.True(vm.IsGameInProgress);
+
+            vm.CancelGameCommand.Execute(null);
+            Assert.False(vm.IsGameInProgress);
+        }
+
+        [Fact]
+        public void SelectingMoveHistory_BlackMove_DisplaysBoardAtSelectedMove()
+        {
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers)
+                    .Move(new Coord(2, 1), new Coord(3, 0))
+                    .Move(new Coord(5, 6), new Coord(4, 7))
+            };
+
+            Assert.NotEqual(vm.Controller.MoveHistory[0].BlackMove.ResultingFen, vm.Controller.Fen);
+
+            vm.MoveHistoryCommand.Execute(vm.Controller.MoveHistory[0].BlackMove.ResultingFen);
+
+            // we are just checking the board--remove the character that says the next move
+            Assert.Equal(vm.Controller.MoveHistory[0].BlackMove.ResultingFen.Remove(6, 1), vm.Controller.Fen.Remove(6, 1));
+        }
+
+        [Fact]
+        public void SelectingMoveHistory_WhiteMove_DisplaysBoardAtSelectedMove()
+        {
+            var vm = new GamePageViewModel
+            {
+                Controller = new GameController(Variant.AmericanCheckers)
+                    .Move(new Coord(2, 1), new Coord(3, 0))
+                    .Move(new Coord(5, 6), new Coord(4, 7))
+                    .Move(new Coord(2, 3), new Coord(3, 2))
+            };
+
+            Assert.NotEqual(vm.Controller.MoveHistory[0].WhiteMove.ResultingFen, vm.Controller.Fen);
+
+            vm.MoveHistoryCommand.Execute(vm.Controller.MoveHistory[0].WhiteMove.ResultingFen);
+
+            // we are just checking the board--remove the character that says the next move
+            Assert.Equal(vm.Controller.MoveHistory[0].WhiteMove.ResultingFen.Remove(6, 1), vm.Controller.Fen.Remove(6, 1));
         }
     }
 }
