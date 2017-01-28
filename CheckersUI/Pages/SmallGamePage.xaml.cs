d3d5f364baacbd31f9c1f5865d600099a3094b0f
Abraham Hosch <hosch250@live.com>
2017-01-28 14:37:53 -0600

    Get the move history and certain app bar commands working

--- CheckersUI/Pages/SmallGamePage.xaml.cs
+++ CheckersUI/Pages/SmallGamePage.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace CheckersUI.Pages
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Input;
+
+namespace CheckersUI.Pages
 {
     public sealed partial class SmallGamePage
     {
         public SmallGamePage()
         {
             InitializeComponent();
+        }
+
+        private void RadioButton_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            var senderElement = sender as FrameworkElement;
+            var flyoutBase = FlyoutBase.GetAttachedFlyout(senderElement);
+            flyoutBase.ShowAt(senderElement);
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            MoveHistory.Visibility = Visibility.Collapsed;
+        }
+
+        private void TextBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MoveHistory.Visibility = Visibility.Visible;
         }
     }
 }
